273f6cc764b6791461a47a2aeaa07b1718e56a4a
Zain Fathoni <me@zainf.dev>
2023-02-22 00:10:03 +0800

    build(tailwind): integrate Tailwind CSS using built-in Tailwind support
    https://remix.run/docs/en/v1/guides/styling#tailwind-css

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/deno";
+import type { LinksFunction, MetaFunction } from "@remix-run/deno";
 import {
   Links,
   LiveReload,
@@ -8,12 +8,15 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import * as React from "react";
+import styles from "./tailwind.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
--- remix.config.js
+++ remix.config.js
@@ -7,6 +7,9 @@ module.exports = {
   If live reload seems slow, try to decrease the dev server broadcast delay.
   */
   devServerBroadcastDelay: 300,
+  future: {
+    unstable_tailwind: true,
+  },
   ignoredRouteFiles: ["**/.*"],
   server: "./server.ts",
   serverConditions: ["deno", "worker"],
